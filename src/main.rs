1f48fe3f6c63ca6e8314c5199d932684b2929cef
Luc <luc@lucemans.nl>
2025-01-26 23:34:39 +0100

    Introduce influx ingest

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use chrono::{DateTime, Utc};
+use futures::stream;
+use influxdb2::models::DataPoint;
+use influxdb2::Client;
+use influxdb2_derive::WriteDataPoint;
 use poem::{
     handler,
     listener::TcpListener,
@@ -34,11 +39,70 @@ struct PushBody {
     samples: String,
 }
 
+#[derive(Debug, Deserialize, Serialize)]
+pub struct IOSHeartrateSample {
+    value: String,
+    #[serde(rename = "type")]
+    _type: Option<String>,
+    #[serde(rename = "unit")]
+    unit: Option<String>,
+    #[serde(rename = "startDate")]
+    start_date: String,
+    #[serde(rename = "endDate")]
+    end_date: String,
+}
+
+#[derive(Debug, Deserialize, Serialize, WriteDataPoint, Default)]
+#[measurement = "heartrate"]
+pub struct HeartrateSample {
+    #[influxdb(field)]
+    value: i64,
+    #[influxdb(tag)]
+    start_date: String,
+    #[influxdb(timestamp)]
+    end_date: i64,
+}
+
+impl From<IOSHeartrateSample> for HeartrateSample {
+    fn from(sample: IOSHeartrateSample) -> Self {
+        Self {
+            value: sample.value.parse::<i64>().unwrap(),
+            start_date: parse_ios_date(&sample.start_date).to_rfc2822(),
+            end_date: parse_ios_date(&sample.end_date).timestamp(),
+        }
+    }
+}
+
+// input: "24 Jan 2025 at 00:05"
+fn parse_ios_date(date: &str) -> DateTime<Utc> {
+    info!("date: {:?}", date);
+
+    date.parse::<DateTime<Utc>>().unwrap()
+}
+
 #[handler]
 async fn push_ios(state: Data<&Arc<AppState>>, body: Json<PushBody>) -> impl IntoResponse {
     let _ = state.config;
 
-    info!("pushing ios {:?}", body);
+    // info!("pushing ios {:?}", body);
+    let client = Client::new(
+        &state.config.influxdb.url,
+        &state.config.influxdb.org,
+        &state.config.influxdb.token,
+    );
+
+    let samples = body.samples.replace("\\\"", "\"");
+    let samples = format!("[{}]", samples);
+    let samples: Vec<IOSHeartrateSample> = serde_json::from_str(&samples).unwrap();
+
+    let samples: Vec<HeartrateSample> = samples.into_iter().map(|s| s.into()).collect();
+
+    info!("samples: {:?}", samples);
+
+    client
+        .write(&state.config.influxdb.bucket, stream::iter(samples))
+        .await
+        .unwrap();
 
     "ok"
 }
